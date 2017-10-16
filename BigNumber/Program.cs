33e790854b1802d1099b72429342aac53b9330df
bastrakov.sergei <bastrakov.sergei@gmail.com>
2017-10-16 14:30:38 +0300

    Initial commit

---
+++ BigNumber/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumber
+{
+
+    public class BigNumber
+    {
+        private readonly List<int> _container = new List<int>();
+
+        public BigNumber(string str)
+        {
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                char c = str[i];
+                _container.Add(c - '0');
+            }
+        }
+
+        private BigNumber(List<int> container)
+        {
+            _container = container;
+        }
+
+        public BigNumber Add(BigNumber other)
+        {
+            int n1 = _container.Count;
+            int n2 = other._container.Count;
+
+            int n = n1 > n2 ? n1 : n2;  
+
+            List<int> resultContainer = new List<int>(n + 1);
+
+            int x = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int a = i < _container.Count ? _container[i] : 0;
+                int b = i < other._container.Count ? other._container[i] : 0;
+
+                int result = a + b + x;
+
+                resultContainer.Add(result % 10);
+                x = result / 10;
+            }
+
+            if (x != 0)
+            {
+                resultContainer.Add(x);
+            }
+
+            return new BigNumber(resultContainer);
+        }
+
+        public override string ToString()
+        {
+            return string.Join("", ((IEnumerable<int>) _container).Reverse());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {                               
+
+        }
+    }
+}
---
+++ BigNumberTests/BigNumberTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BigNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumber.Tests
+{
+    [TestClass()]
+    public class BigNumberTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            BigNumber number = new BigNumber("123456");
+
+            string actual = number.ToString();
+
+            Assert.AreEqual("123456", actual);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            BigNumber a = new BigNumber("123456");
+            BigNumber b = new BigNumber("654312");
+
+            BigNumber actual = a.Add(b);
+
+            Assert.AreEqual("777768", actual.ToString());
+        }
+
+        [TestMethod()]
+        public void AddTest2()
+        {
+            BigNumber a = new BigNumber("123");
+            BigNumber b = new BigNumber("12");
+
+            BigNumber actual = a.Add(b);
+
+            Assert.AreEqual("135", actual.ToString());
+        }
+
+        [TestMethod()]
+        public void AddTest3()
+        {
+            BigNumber a = new BigNumber("12");
+            BigNumber b = new BigNumber("123");
+
+            BigNumber actual = a.Add(b);
+
+            Assert.AreEqual("135", actual.ToString());
+        }
+
+        [TestMethod()]
+        public void AddTest4()
+        {
+            BigNumber a = new BigNumber("99");
+            BigNumber b = new BigNumber("1");
+
+            BigNumber actual = a.Add(b);
+
+            Assert.AreEqual("100", actual.ToString());
+        }
+
+        [TestMethod()]
+        public void AddTest5()
+        {
+            BigNumber a = new BigNumber("199");
+            BigNumber b = new BigNumber("1");
+
+            BigNumber actual = a.Add(b);
+
+            Assert.AreEqual("200", actual.ToString());
+        }
+    }
+}
